d910f5ffb9c2c69bae432981b2757dbbf5d6ba0f
DESKTOP-JONQLF3\thaiq <quanghuy.thai@sick.com>
2024-09-03 23:42:27 +0700

    feat(backend): setup static serve

--- backend/internal/api/api.go
+++ backend/internal/api/api.go
@@ -5,6 +5,7 @@ import (
 	"db-tool/internal/core/middlewares"
 	"db-tool/internal/routes/users"
 	"fmt"
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"os"
 )
@@ -15,17 +16,18 @@ func Init() {
 	port := os.Getenv("PORT")
 	//Setup GIN
 	r := gin.Default()
+
+	r.Use(static.Serve("/", static.LocalFile("web", false)))
 	r.Use(middlewares.Logger())
 	r.Use(interceptors.JsonApiInterceptor())
-	//Serving static
-
-	//
 
 	//Serving API
 	api = r.Group("/api")
 
 	//Register modules
 	users.Register(api)
+
+	//Start the Server
 	fmt.Println("Server is running on port: ", port)
 	err := r.Run(":" + port)
 	if err != nil {
