e0a968354f059328689c78a99bc72bdb4f8cf2e3
huykuka <87870621+huykuka@users.noreply.github.com>
2024-09-08 18:38:52 +0700

    setup(frontend):done setup frontend with nx and shadcn-ui

--- frontend/src/app/app.tsx
+++ frontend/src/app/app.tsx
@@ -1,9 +1,19 @@
-import NxWelcome from './nx-welcome';
 
+import {ThemeProvider} from "./components";
+import {Accordion, AccordionContent, AccordionItem, AccordionTrigger} from "@frontend/shared/components/ui/accordion";
 export function App() {
   return (
     <div>
-      <NxWelcome title="frontend" />
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Accordion type="single" collapsible>
+                <AccordionItem value="item-1">
+                    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+                    <AccordionContent>
+                        Yes. It adheres to the WAI-ARIA design pattern.
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
+        </ThemeProvider>
     </div>
   );
 }
