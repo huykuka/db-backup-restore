d7b8d648d6f4b02d1b1d043bdf18e02f440ecb12
huykuka <thaiquanghuy.c3a@gmail.com>
2024-09-10 09:34:01 +0700

    feat(frontend): fix light mode

--- frontend/src/app/components/core/mode-toggle.tsx
+++ frontend/src/app/components/core/mode-toggle.tsx
@@ -1,4 +1,4 @@
-import { Moon, Sun } from "lucide-react"
+import {Moon, Sun} from "lucide-react"
 import {useTheme} from "./theme-provider";
 import {
     DropdownMenu,
@@ -9,14 +9,16 @@ import {
 import {Button} from "@frontend/shared/components/ui/button";
 
 export function ModeToggle() {
-    const { setTheme } = useTheme()
+    const {setTheme} = useTheme()
 
     return (
         <DropdownMenu>
             <DropdownMenuTrigger asChild>
                 <Button variant="outline" size="icon">
-                    <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all text-black dark:-rotate-90 dark:scale-0" />
-                    <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <Sun
+                        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
+                    <Moon
+                        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
                     <span className="sr-only">Toggle theme</span>
                 </Button>
             </DropdownMenuTrigger>
