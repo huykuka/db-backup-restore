cefc0e62d1b163fb858477020f832606f4c66927
huykuka <thaiquanghuy.c3a@gmail.com>
2024-10-14 21:23:39 +0700

    feat(frontend): add charts

--- frontend/src/app/routes/home.tsx
+++ frontend/src/app/routes/home.tsx
@@ -1,22 +1,24 @@
-import React from 'react';
+import { Separator } from "@frontend/shared/components/ui/separator";
+import { HistoryComponent } from "../components/history/history";
 import Settings from "../components/settings/settings";
-import {Separator} from "@frontend/shared/components/ui/separator";
-import {HistoryComponent} from "../components/history/history";
 
 
 const Home = () => {
     return (
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 w-full">
-            <div className="md:col-span-1">
-                <Settings/>
-            </div>
+        <div className="flex flex-col gap-5">
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-5 w-full">
 
-            <div className="block md:hidden">
-                <Separator/>
-            </div>
+                <div className="md:col-span-1">
+                    <Settings />
+                </div>
 
-            <div className="md:col-span-2">
-                <HistoryComponent/>
+                <div className="block md:hidden">
+                    <Separator />
+                </div>
+
+                <div className="md:col-span-2">
+                    <HistoryComponent />
+                </div>
             </div>
         </div>
     );
