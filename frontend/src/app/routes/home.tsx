e7b966ac5f6bf704ac988e8111133bec57439561
huykuka <thaiquanghuy.c3a@gmail.com>
2024-10-14 22:25:18 +0700

    feat(frontend): add chart demo

--- frontend/src/app/routes/home.tsx
+++ frontend/src/app/routes/home.tsx
@@ -1,3 +1,5 @@
+import { BarChartComponent } from '@components/core/charts/bar-chart';
+import { PieChartComponent } from '@components/core/charts/pie-chart';
 import { Separator } from "@frontend/shared/components/ui/separator";
 import { HistoryComponent } from "../components/history/history";
 import Settings from "../components/settings/settings";
@@ -6,6 +8,15 @@ import Settings from "../components/settings/settings";
 const Home = () => {
     return (
         <div className="flex flex-col gap-5">
+            <div className="flex flex-row gap-4 ">
+                <div className="flex flex-row gap-4 h-fit">
+                    <PieChartComponent></PieChartComponent>
+                    <PieChartComponent></PieChartComponent>
+                    <PieChartComponent></PieChartComponent>
+                </div>
+                <BarChartComponent />
+            </div>
+            <Separator></Separator>
             <div className="grid grid-cols-1 md:grid-cols-3 gap-5 w-full">
 
                 <div className="md:col-span-1">
