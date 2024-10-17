517c73ba8c2e1c4618bd59daa7ca5ad7d2681daf
huykuka <thaiquanghuy.c3a@gmail.com>
2024-10-17 14:11:05 +0700

    feat(frontend): add file upload table

--- frontend/src/app/components/manual-upload/manual-upload.tsx
+++ frontend/src/app/components/manual-upload/manual-upload.tsx
@@ -1,19 +1,37 @@
 import { useState } from 'react';
 import FileUpload from '../core/file-upload';
-import manualUploadService from './manual-upload.service';
+import manualUploadService, { useManualUpload } from './manual-upload.service';
+import { UploadFileTable } from './upload-file-table';
+
 
 export default function ManualFileUpload() {
+  useManualUpload()
+
   const [uploadProgress, setUploadProgress] = useState<number>(0);
 
+  const { getState, setState } = manualUploadService
+
+  const handleDeleteFile = (path: String) => {
+    const updatedFiles = getState().files.filter(file => file.filePath !== path);
+    setState('files', updatedFiles);
+  }
+
+  const handleRestoreFile = (path: string) => {
+    console.log(path)
+  }
+
   return (
-    <FileUpload
-      acceptedTypes={['.sql', '.psql', '.zip', '.MOV']}
-      title="Manual Backup Upload"
-      description="Upload your .psql file to restore the database."
-      uploadProgress={uploadProgress}
-      onFileUpload={async (file: any) => {
-        await manualUploadService.upload(file, setUploadProgress);
-      }}
-    />
+    <div className='flex flex-col gap-4'>
+      <FileUpload
+        acceptedTypes={['.sql', '.psql', '.zip', '.MOV']}
+        title="Manual Backup Upload"
+        description="Upload your .psql file to restore the database."
+        uploadProgress={uploadProgress}
+        onFileUpload={async (file: any) => {
+          await manualUploadService.upload(file, setUploadProgress);
+        }}
+      />
+      <UploadFileTable onDeleteFile={handleDeleteFile} onRestoreFile={handleRestoreFile} />
+    </div>
   );
 }
