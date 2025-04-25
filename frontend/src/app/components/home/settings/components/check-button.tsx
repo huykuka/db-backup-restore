3e0d302a2c4a79bd0b6d4ed605aa285fbe34bdf9
huykuka <thaiquanghuy.c3a@gmail.com>
2025-04-25 09:45:08 +0700

    Add volume mount for data persistence and fix import paths

--- frontend/src/app/components/home/settings/components/check-button.tsx
+++ frontend/src/app/components/home/settings/components/check-button.tsx
@@ -6,42 +6,43 @@ import { useFormContext } from 'react-hook-form';
 import { toast } from 'sonner';
 
 export const CheckButton = () => {
-    const [checking, setChecking] = useState<boolean>(false);
-    const [success, setSuccess] = useState<boolean>(false);
-    const { formState } = useFormContext();
+  const [checking, setChecking] = useState<boolean>(false);
+  const [success, setSuccess] = useState<boolean>(false);
+  const { formState } = useFormContext();
 
-    async function onVerifyConnection() {
-        try {
-            setChecking(true);
-            setSuccess(false);
-            const response = await healthService.getDatabaseHealth();
-            const status = response.data.message === 'ok';
-            setTimeout(() => {
-                if (status) {
-                    setSuccess(true);
-                    toast.success('Connection verified');
-                }
-                setChecking(false);
-            }, 2000);
-        } catch (e) {
-            setSuccess(false);
-            setChecking(false);
+  async function onVerifyConnection() {
+    try {
+      setChecking(true);
+      setSuccess(false);
+      const response = await healthService.getDatabaseHealth();
+      const status = response.data.message === 'ok';
+      setTimeout(() => {
+        if (status) {
+          setSuccess(true);
+          toast.success('Connection verified');
         }
+        setChecking(false);
+      }, 2000);
+    } catch (e) {
+      setSuccess(false);
+      toast.success('Connection failed');
+      setChecking(false);
     }
+  }
 
-    return (
-        <Button
-            className="w-1/2"
-            variant="outline"
-            type="button"
-            disabled={!formState.isValid}
-            onClick={onVerifyConnection}
-        >
-            {success && (
-                <Check className="mr-2 text-green-600 transition-opacity opacity-100 duration-150" />
-            )}
-            {checking && <Loader className="w-5 animate-spin mr-2" />}
-            <span>Verify</span>
-        </Button>
-    );
+  return (
+    <Button
+      className="w-1/2"
+      variant="outline"
+      type="button"
+      disabled={!formState.isValid}
+      onClick={onVerifyConnection}
+    >
+      {success && (
+        <Check className="mr-2 text-green-600 transition-opacity opacity-100 duration-150" />
+      )}
+      {checking && <Loader className="w-5 animate-spin mr-2" />}
+      <span>Verify</span>
+    </Button>
+  );
 };
